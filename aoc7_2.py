5c2c9bfb670e5d4c49dbdcbab88ed5e0df7ee3f4
Mark <markd315@gmail.com>
2024-12-07 11:18:49 -0500

    day 7 morning solution recursive

---
+++ aoc7_2.py
@@ -0,0 +1,35 @@
+
+
+
+import functools
+from collections import defaultdict
+from copy import copy
+
+
+def evaluate(equals, in_list):
+    if len(in_list) == 2:
+        return in_list[0] * in_list[1] == equals or in_list[0] + in_list[1] == equals  or str(in_list[0]) + str(in_list[1]) == str(equals)
+    mult = copy(in_list)
+    cat = copy(in_list)
+    mult[1] *= mult[0]
+    cat[1] = int(str(cat[0]) + str(cat[1]))
+    in_list[1] += in_list[0]
+    return evaluate(equals, mult[1:]) or evaluate(equals, in_list[1:]) or evaluate(equals, cat[1:])
+
+# initialize
+in_list = []
+# read
+with open("input/7.txt") as f:
+    lines = f.readlines()
+for line in lines:
+    nums = []
+    for n in line.strip().split(" "):
+        nums.append(int(n.replace(":", "")))
+    in_list.append(nums)
+# process
+# answer
+ans = 0
+for book in in_list:
+    equals = book[0]
+    ans = ans + equals if evaluate(equals, book[1:]) else ans
+print(ans)
---
+++ aoc7_recursive_ops.py
@@ -0,0 +1,30 @@
+import functools
+from collections import defaultdict
+from copy import copy
+
+
+def evaluate(equals, in_list):
+    if len(in_list) == 2:
+        return in_list[0] * in_list[1] == equals or in_list[0] + in_list[1] == equals
+    mult = copy(in_list)
+    mult[1] *= mult[0]
+    in_list[1] += in_list[0]
+    return evaluate(equals, mult[1:]) or evaluate(equals, in_list[1:])
+
+# initialize
+in_list = []
+# read
+with open("input/7.txt") as f:
+    lines = f.readlines()
+for line in lines:
+    nums = []
+    for n in line.strip().split(" "):
+        nums.append(int(n.replace(":", "")))
+    in_list.append(nums)
+# process
+# answer
+ans = 0
+for book in in_list:
+    equals = book[0]
+    ans = ans + equals if evaluate(equals, book[1:]) else ans
+print(ans)
