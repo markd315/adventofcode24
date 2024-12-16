96daaa52a6e5a473b057c63dbcf179c5b20feedc
Mark <markd315@gmail.com>
2024-12-16 14:29:07 -0500

    aoc 16_2 debugging

--- aoc16_2.py
+++ aoc16_2.py
@@ -1,42 +1,94 @@
-import functools
+import collections
 from collections import defaultdict
+from copy import copy
 
-def comparator(inp, two):
-    if inp in map.keys():
-        if two in map[inp]:
-            return -1
-    if two in map.keys():
-        if inp in map[two]:
-            return 1
-    return 0
 
-# initialize
-a = []
-b = []
-#read
+class T(tuple):
+    def __add__(self, other):
+        return T(x + y for x, y in zip(self, other))
+
+    def rot(self):
+        x, y = self
+        return T((y, -x))
+
+
+NORTH = T((-1, 0))
+EAST = NORTH.rot()
+SOUTH = EAST.rot()
+WEST = SOUTH.rot()
+modifications = [SOUTH,WEST,NORTH,EAST]
+
 with open("input/16.txt") as f:
     lines = f.readlines()
-#parse (two stage)
-stage_two_parse = False
+# parse (two stage)
 in_list = []
-map = defaultdict(list)
-for line in lines:
-    if line.strip() == "":
-        stage_two_parse = True
-        continue
-    if not stage_two_parse:
-        x = line.split("|")
-        a_x = int(x[0])
-        b_x = int(x[1].strip())
-        map[a_x].append(b_x)
-    else:
-        nums = []
-        for n in line.strip().split(","):
-            nums.append(int(n))
-        in_list.append(nums)
-#process
-#answer
+r_y, r_x = 0, 0
+scores = defaultdict(int) # (y, x) -> int score
+paths = defaultdict(list) # dict: T(y, x) -> [{},{}]
+dir = {}
+for idy, line in enumerate(lines):
+    vals = []
+    for idx, num in enumerate(line.strip()):
+        if num == "S":
+            r_y = idy
+            r_x = idx
+        if num == "E":
+            e_y = idy
+            e_x = idx
+        vals.append(num)
+    in_list.append(vals)
+# process
+# answer
+
 ans = 0
-sorted(in_list, key=functools.cmp_to_key(comparator), reverse=True)
-for book in in_list:
-    
+
+scores[T((r_y, r_x))] = 0
+paths[T((r_y, r_x))] = [{T((r_y, r_x))}] #todo array of set for p2
+frz = frozenset({T((r_y, r_x))})
+facing = {frz: EAST} # dict: path -> dir
+queue = collections.deque()
+queue.append(T((r_y, r_x)))
+best_paths = []
+while len(queue) > 0:
+    elem = queue.popleft()
+    for path in paths[elem]:
+        t_dir = facing[frozenset(path)]
+        old_score = scores[elem]
+        for dirs in [t_dir, t_dir.rot(), t_dir.rot().rot().rot()]:
+            addl_score = 1 if dirs == t_dir else 1001
+            new_loc = elem + dirs
+            curr_path = copy(path)
+            curr_path.add(new_loc)
+            frz = frozenset(curr_path)
+            if frz in paths[new_loc]: # short circuit cycles
+                continue
+            y, x = new_loc[0], new_loc[1]
+            if in_list[y][x] != "#":
+                score = old_score + addl_score
+                if scores[T((y, x))] == 0 or score <= scores[T((y, x))]:
+                    if y == 7 and x == 15: # todo
+                        # junction is wrong because to GET to 7,15 is cheaper from the left but to PROGRESS from there is same
+                        print("break")
+                    if score < scores[T((y,x))] or scores[T((y, x))] == 0:
+                        paths[new_loc] = [curr_path]
+                    else:
+                        paths[new_loc].append(curr_path)
+                    queue.append(new_loc)
+                    facing[frz] = dirs
+                    scores[T((y, x))] = score
+
+ans = scores[T((e_y, e_x))]
+total_set = set()
+for path in paths[T((e_y, e_x))]:
+    for elem in path:
+        total_set.add(elem)
+print(len(total_set))
+for y, line in enumerate(in_list):
+    out = ""
+    for x, char in enumerate(line):
+        if T((y, x)) in total_set:
+            out += "0"
+        else:
+            out += char
+    print(out)
+#print(ans)
