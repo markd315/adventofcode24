1fc009f1f469a524bab0b2935de57b78e0915d78
Mark <markd315@gmail.com>
2024-12-16 15:58:56 -0500

    wrong answer 41

--- aoc16_2.py
+++ aoc16_2.py
@@ -23,7 +23,7 @@ with open("input/16.txt") as f:
 # parse (two stage)
 in_list = []
 r_y, r_x = 0, 0
-scores = defaultdict(int) # (y, x) -> int score
+scores = defaultdict(lambda: float('inf')) # (y, x) -> int score
 paths = defaultdict(list) # dict: T(y, x) -> [{},{}]
 dir = {}
 for idy, line in enumerate(lines):
@@ -42,40 +42,50 @@ for idy, line in enumerate(lines):
 
 ans = 0
 
-scores[T((r_y, r_x))] = 0
+scores[T((r_y, r_x, EAST))] = 0
 paths[T((r_y, r_x))] = [{T((r_y, r_x))}] #todo array of set for p2
 frz = frozenset({T((r_y, r_x))})
 facing = {frz: EAST} # dict: path -> dir
 queue = collections.deque()
 queue.append(T((r_y, r_x)))
 best_paths = []
+visited = set()
 while len(queue) > 0:
     elem = queue.popleft()
     for path in paths[elem]:
         t_dir = facing[frozenset(path)]
-        old_score = scores[elem]
         for dirs in [t_dir, t_dir.rot(), t_dir.rot().rot().rot()]:
-            addl_score = 1 if dirs == t_dir else 1001
+            old_score = scores[(elem[0], elem[1], t_dir)]
             new_loc = elem + dirs
+            y, x = new_loc[0], new_loc[1]
+            #if (y, x, dirs) not in visited:
+            #    visited.add((y, x, dirs))
+            #else:
+            #    continue
+            if in_list[y][x] == "#":
+                continue
+            score = old_score
+            score += 1 if dirs == t_dir else 1001
+            if score >= scores[(y, x, dirs)]:
+                continue
+            if frz in paths[new_loc]:  # short circuit cycles
+                continue
             curr_path = copy(path)
             curr_path.add(new_loc)
             frz = frozenset(curr_path)
-            if frz in paths[new_loc]: # short circuit cycles
-                continue
-            y, x = new_loc[0], new_loc[1]
-            if in_list[y][x] != "#":
-                score = old_score + addl_score
-                if scores[T((y, x))] == 0 or score <= scores[T((y, x))]:
-                    if y == 7 and x == 15: # todo
-                        # junction is wrong because to GET to 7,15 is cheaper from the left but to PROGRESS from there is same
-                        print("break")
-                    if score < scores[T((y,x))] or scores[T((y, x))] == 0:
-                        paths[new_loc] = [curr_path]
-                    else:
-                        paths[new_loc].append(curr_path)
-                    queue.append(new_loc)
-                    facing[frz] = dirs
-                    scores[T((y, x))] = score
+            if score <= scores[(y, x, t_dir)]:
+                if y == 7 and x == 15: # todo
+                    # junction is wrong because to GET to 7,15 is cheaper from the left but to PROGRESS from there is same
+                    print("break")
+                    # [score for score in scores if score[1] == 15 and score[0] == 7]
+                    # scores[[score for score in scores if score[1] == 15 and score[0] == 7][0]]
+                if score < scores[(y,x, dirs)]:
+                    paths[new_loc] = [curr_path]
+                    scores[(y, x, dirs)] = score
+                else:
+                    paths[new_loc].append(curr_path)
+                queue.append(new_loc)
+                facing[frz] = dirs
 
 ans = scores[T((e_y, e_x))]
 total_set = set()
