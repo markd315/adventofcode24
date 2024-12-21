26b4b388e15d6626aa612c2e8b29d000e5978470
Mark <markd315@gmail.com>
2024-12-21 12:25:01 -0500

    still last input wrong, start working towards multi try solution

--- aoc21.py
+++ aoc21.py
@@ -43,7 +43,7 @@ dgrid_orders = {
     T((0, 1)): "udlr",
     T((0,2)): "urdl",
     T((1,0)): "ldur",
-    T((1,1)): "dulr",
+    T((1,1)): "dlru",
     T((1,2)): "rdul",
 }
 
@@ -57,35 +57,64 @@ def p_dir(dir1, in_cursor, next_cursor):
     target = find_in_grid(d_grid, dir1)
     diff = target - in_cursor
     y, x = diff
-    order = dgrid_orders[next_cursor]
-    steps = resolve_ops(order, y, x)
-    steps.append('a')
+    orders = [dgrid_orders[next_cursor]]
+    if y > 0 and x > 0:
+        orders = ["dr", "rd"]
+    if x < 0 and y < 0:
+        orders = ["lu", "ul"]
+    if y > 0 and x < 0:
+        orders = ["dl", "ld"]
+    if y < 0 and x > 0:
+        orders = ["ur", "ru"]
+    if dir1 in "ua" and in_cursor == T((1,0)):
+        orders = "ru"
+    if dir1 == "l" and in_cursor in [T((0,1)),T((0,2))]:
+        orders = "dl"
+    steps = resolve_ops(orders, y, x)
+    for seq in steps:
+        seq.append('a')
     return steps, target
 
-def resolve_ops(order, y, x):
-    steps = []
-    for elem in order:
-        while y > 0 and elem == "d":
-            steps.append("d")
-            y -= 1
-        while y < 0 and elem == "u":
-            steps.append("u")
-            y += 1
-        while x > 0 and elem == "r":
-            steps.append("r")
-            x -= 1
-        while x < 0 and elem == "l":
-            steps.append("l")
-            x += 1
-    return steps
+def resolve_ops(orders, y, x):
+    seqs = []
+    for ordering in orders:
+        steps = []
+        for elem in ordering:
+            while y > 0 and elem == "d":
+                steps.append("d")
+                y -= 1
+            while y < 0 and elem == "u":
+                steps.append("u")
+                y += 1
+            while x > 0 and elem == "r":
+                steps.append("r")
+                x -= 1
+            while x < 0 and elem == "l":
+                steps.append("l")
+                x += 1
+        seqs.append(steps)
+    return seqs
 
 def p_num(numerical, n_cursor, next_cursor):
     target = find_in_grid(f_grid, numerical)
     diff =  target - n_cursor
     y, x = diff
-    order = dgrid_orders[next_cursor]
-    steps = resolve_ops(order, y, x)
-    steps.append('a')
+    orders = [dgrid_orders[next_cursor]]
+    if y > 0 and x > 0:
+        orders = ["dr", "rd"]
+    if x < 0 and y < 0:
+        orders = ["lu", "ul"]
+    if y > 0 and x < 0:
+        orders = ["dl", "ld"]
+    if y < 0 and x > 0:
+        orders = ["ur", "ru"]
+    if numerical in "741" and y < 0 and x < 0: # avoid blank spaces
+        orders = ["lu"]
+    if numerical in "0A" and y > 0 and x > 0:
+        orders = ["dr"]
+    steps = resolve_ops(orders, y, x)
+    for seq in steps:
+        seq.append('a')
     return steps, target
 
 def press(book, n_cursor, d1_cursor, d2_cursor):
